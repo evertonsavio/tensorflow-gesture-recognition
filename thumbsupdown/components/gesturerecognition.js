d5da5bea6bf9d07ff2c48df576d8e206a0c0b9b2
Everton Sávio S. Lucas <everluca@outlook.com>
2020-07-30 14:03:23 -0300

    Inner Text

--- thumbsupdown/components/gesturerecognition.js
+++ thumbsupdown/components/gesturerecognition.js
@@ -18,140 +18,143 @@ const gestureStrings = {
   thumbs_down: '👎',
 };
 
-async function main() {
-  let fileInput = document.getElementById('fileinput');
-  fileInput.addEventListener('change', function (ev) {
+async function main () {
+  let fileInput = document.getElementById ('fileinput');
+  fileInput.addEventListener ('change', function (ev) {
     if (ev.target.files) {
       let file = ev.target.files[0];
-      var reader = new FileReader();
-      reader.readAsDataURL(file);
+      var reader = new FileReader ();
+      reader.readAsDataURL (file);
       reader.onloadend = function (e) {
-        var image = new Image();
+        var image = new Image ();
         image.src = e.target.result;
         image.onload = function (ev) {
-          var canvas = document.getElementById('canvas');
+          var canvas = document.getElementById ('canvas');
           canvas.width = 640; //image.width;
           canvas.height = 480; //image.height;
-          var ctx = canvas.getContext('2d');
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.drawImage(image, 0, 0, 640, 480);
+          var ctx = canvas.getContext ('2d');
+          ctx.clearRect (0, 0, canvas.width, canvas.height);
+          ctx.drawImage (image, 0, 0, 640, 480);
         };
       };
     }
   });
 
-  const video = document.querySelector('#pose-video');
-  const canvas = document.querySelector('#pose-canvas');
-  const ctx = canvas.getContext('2d');
+  const video = document.querySelector ('#pose-video');
+  const canvas = document.querySelector ('#pose-canvas');
+  const ctx = canvas.getContext ('2d');
 
-  const resultLayer = document.querySelector('#pose-result');
+  const resultLayer = document.querySelector ('#pose-result');
+  const infoText = document.querySelector ('#info-text');
 
-  const thumbsUpGesture = new fp.GestureDescription('thumbs_up');
-  thumbsUpGesture.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+  const thumbsUpGesture = new fp.GestureDescription ('thumbs_up');
+  thumbsUpGesture.addCurl (fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
 
-  thumbsUpGesture.addDirection(
+  thumbsUpGesture.addDirection (
     fp.Finger.Thumb,
     fp.FingerDirection.VerticalUp,
     1.0
   );
-  thumbsUpGesture.addDirection(
+  thumbsUpGesture.addDirection (
     fp.Finger.Thumb,
     fp.FingerDirection.DiagonalUpLeft,
     0.5
   );
-  thumbsUpGesture.addDirection(
+  thumbsUpGesture.addDirection (
     fp.Finger.Thumb,
     fp.FingerDirection.DiagonalUpRight,
     0.5
   );
-  thumbsUpGesture.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1.0);
-  thumbsUpGesture.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1.0);
-  thumbsUpGesture.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1.0);
-  thumbsUpGesture.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1.0);
+  thumbsUpGesture.addCurl (fp.Finger.Index, fp.FingerCurl.FullCurl, 1.0);
+  thumbsUpGesture.addCurl (fp.Finger.Middle, fp.FingerCurl.FullCurl, 1.0);
+  thumbsUpGesture.addCurl (fp.Finger.Ring, fp.FingerCurl.FullCurl, 1.0);
+  thumbsUpGesture.addCurl (fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1.0);
 
-  const thumbsDownGesture = new fp.GestureDescription('thumbs_down');
+  const thumbsDownGesture = new fp.GestureDescription ('thumbs_down');
 
-  thumbsDownGesture.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
-  thumbsDownGesture.addDirection(
+  thumbsDownGesture.addCurl (fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+  thumbsDownGesture.addDirection (
     fp.Finger.Thumb,
     fp.FingerDirection.VerticalDown,
     1.0
   );
-  thumbsDownGesture.addDirection(
+  thumbsDownGesture.addDirection (
     fp.Finger.Thumb,
     fp.FingerDirection.DiagonalDownLeft,
     0.5
   );
-  thumbsDownGesture.addDirection(
+  thumbsDownGesture.addDirection (
     fp.Finger.Thumb,
     fp.FingerDirection.DiagonalDownRight,
     0.5
   );
 
-  thumbsDownGesture.addCurl(fp.Finger.Index, fp.FingerCurl.FullCurl, 1.0);
-  thumbsDownGesture.addCurl(fp.Finger.Middle, fp.FingerCurl.FullCurl, 1.0);
-  thumbsDownGesture.addCurl(fp.Finger.Ring, fp.FingerCurl.FullCurl, 1.0);
-  thumbsDownGesture.addCurl(fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1.0);
+  thumbsDownGesture.addCurl (fp.Finger.Index, fp.FingerCurl.FullCurl, 1.0);
+  thumbsDownGesture.addCurl (fp.Finger.Middle, fp.FingerCurl.FullCurl, 1.0);
+  thumbsDownGesture.addCurl (fp.Finger.Ring, fp.FingerCurl.FullCurl, 1.0);
+  thumbsDownGesture.addCurl (fp.Finger.Pinky, fp.FingerCurl.FullCurl, 1.0);
 
   const knownGestures = [thumbsUpGesture, thumbsDownGesture];
-  const GE = new fp.GestureEstimator(knownGestures);
+  const GE = new fp.GestureEstimator (knownGestures);
 
-  const model = await handpose.load();
-  console.log('Handpose model loaded');
+  const model = await handpose.load ();
+  console.log ('Handpose model loaded');
+  infoText.innerText =
+    'Modelo Handpose carregado com sucesso, iniciando predições ✔';
 
   const estimateHands = async () => {
-    ctx.clearRect(0, 0, config.video.width, config.video.height);
-    resultLayer.innerText = '';
+    ctx.clearRect (0, 0, config.video.width, config.video.height);
+    resultLayer.innerText = '⭕';
 
-    const canvasimg = document.querySelector('canvas');
-    const canvasctx = canvasimg.getContext('2d');
+    const canvasimg = document.querySelector ('canvas');
+    const canvasctx = canvasimg.getContext ('2d');
 
-    var imageData = canvasctx.getImageData(
+    var imageData = canvasctx.getImageData (
       0,
       0,
       canvasimg.width,
       canvasimg.height
     );
 
-    console.log(imageData);
+    console.log (imageData);
 
-    if (document.getElementById('option1').checked) {
+    if (document.getElementById ('option1').checked) {
       dataToPredict = video;
     } else {
       dataToPredict = imageData;
     }
 
-    const predictions = await model.estimateHands(dataToPredict, true);
+    const predictions = await model.estimateHands (dataToPredict, true);
 
     for (let i = 0; i < predictions.length; i++) {
       for (let part in predictions[i].annotations) {
         for (let point of predictions[i].annotations[part]) {
-          drawPoint(ctx, point[0], point[1], 3, landmarkColors[part]);
+          drawPoint (ctx, point[0], point[1], 3, landmarkColors[part]);
         }
       }
 
-      const est = GE.estimate(predictions[i].landmarks, 5.5);
+      const est = GE.estimate (predictions[i].landmarks, 5.5);
 
       if (est.gestures.length > 0) {
-        let result = est.gestures.reduce((p, c) => {
+        let result = est.gestures.reduce ((p, c) => {
           return p.confidence > c.confidence ? p : c;
         });
-        console.log(gestureStrings[result.name]);
+        console.log (gestureStrings[result.name]);
 
         resultLayer.innerText = gestureStrings[result.name];
       }
     }
 
-    setTimeout(() => {
-      estimateHands();
+    setTimeout (() => {
+      estimateHands ();
     }, 10000 / config.video.fps);
   };
 
-  estimateHands();
-  console.log('Starting predictions');
+  estimateHands ();
+  console.log ('Starting predictions');
 }
 
-async function initCamera(width, height, fps) {
+async function initCamera (width, height, fps) {
   const constraints = {
     audio: false,
     video: {
@@ -162,44 +165,49 @@ async function initCamera(width, height, fps) {
     },
   };
 
-  const video = document.querySelector('#pose-video');
+  const video = document.querySelector ('#pose-video');
   video.width = width;
   video.height = height;
 
-  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+  const stream = await navigator.mediaDevices.getUserMedia (constraints);
   video.srcObject = stream;
 
-  return new Promise((resolve) => {
+  return new Promise (resolve => {
     video.onloadedmetadata = () => {
-      resolve(video);
+      resolve (video);
     };
   });
 }
 
-function drawPoint(ctx, x, y, r, color) {
-  ctx.beginPath();
-  ctx.arc(x, y, r, 0, 2 * Math.PI);
+function drawPoint (ctx, x, y, r, color) {
+  ctx.beginPath ();
+  ctx.arc (x, y, r, 0, 2 * Math.PI);
   ctx.fillStyle = color;
-  ctx.fill();
+  ctx.fill ();
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('btnApp').style.visibility = 'visible';
+window.addEventListener ('DOMContentLoaded', () => {
+  document.getElementById ('btnApp').style.visibility = 'visible';
 });
 
 const iniciarApp = () => {
-  initCamera(config.video.width, config.video.height, config.video.fps).then(
-    (video) => {
-      video.play();
-      video.addEventListener('loadeddata', (event) => {
-        console.log('Camera is ready');
-        main();
-      });
-    }
-  );
+  const info = document.querySelector ('#info-text');
+  info.innerText = 'Carregando o modelo, por favor aguarde! 🛑';
 
-  const canvas = document.querySelector('#pose-canvas');
+  initCamera (
+    config.video.width,
+    config.video.height,
+    config.video.fps
+  ).then (video => {
+    video.play ();
+    video.addEventListener ('loadeddata', event => {
+      console.log ('Camera is ready');
+      main ();
+    });
+  });
+
+  const canvas = document.querySelector ('#pose-canvas');
   canvas.width = config.video.width;
   canvas.height = config.video.height;
-  console.log('Canvas initialized');
+  console.log ('Canvas initialized');
 };
